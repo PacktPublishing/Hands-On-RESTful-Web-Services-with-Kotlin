2d92deff13806d56c158c9f0a74c1ca356f5702c
Budi Oktaviyan Suryanto <budioktaviyans@gmail.com>
2018-12-22 13:50:54 +0700

    Implement coroutine basic sample

--- Chapter01/build.gradle.kts
+++ Chapter01/build.gradle.kts
@@ -16,6 +16,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.1.0")
 }
 
 application {
