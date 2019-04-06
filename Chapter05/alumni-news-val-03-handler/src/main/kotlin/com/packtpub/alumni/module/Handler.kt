861c0e6121c75c4ed6e7e2a6e4d81f642089f3e4
Deny Prasetyo <jasoet87@gmail.com>
2019-04-06 16:12:22 +0700

    Add UniversityHandler to handlerModule

--- Chapter05/alumni-news-val-03-handler/src/main/kotlin/com/packtpub/alumni/module/Handler.kt
+++ Chapter05/alumni-news-val-03-handler/src/main/kotlin/com/packtpub/alumni/module/Handler.kt
@@ -1,13 +1,17 @@
 package com.packtpub.alumni.module
 
 import com.packtpub.alumni.handler.CityHandler
-import com.packtpub.alumni.model.query.QCity
+import com.packtpub.alumni.handler.UniversityHandler
 import org.koin.dsl.module
 
 val handlerModule = module {
 
     single {
         CityHandler(get())
+    }
+
+    single {
+        UniversityHandler(get())
     }
 
 }
